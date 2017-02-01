0071977442c5854d70171c533ccfd54152ad35ce
dhraman <dharika.raman@capgemini.com>
2017-02-01 17:19:52 +0530

    Add files via upload
    terraform plan
    terraform apply

---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "region"{
+        default = "us-west-2"
+}
+variable "availability_zone" {
+        default = "us-west-2a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-30e65350"
+}
+
+variable "key_name" {
+        default = "proj1"
+}
+
+variable "aws_access_key" {
+        default= "AKIAJ652W4LXTIALB5GQ"
+}
+
+variable "aws_secret_key" {
+        default = "5ZJ3F7vLY/UF+jRZwGcQnZjpR+sUZjs4yyjEzx+q"
+}
+variable "subnet_id" {
+	default = "subnet-b74084fe"
+}  
+
